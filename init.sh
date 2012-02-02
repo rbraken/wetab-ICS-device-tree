0ef759451c7f82c60ed7ecbc3327a7c75268a4e2
Rene van den Braken <rene.van.den.braken@gmail.com>
2012-02-02 15:10:35 +0100

    Use a modified version of the android-x86 alsa based libaudio. Modification was needed for the microphone. Also set some beter mixer defaults.
    It is build as audio.primary.wetab.so so it will overwrite the default

--- init.sh
+++ init.sh
@@ -1,7 +1,18 @@
 #!/system/bin/sh
 #cp /system/bin/su /system/bin/su.bk
-#cp /system/xbin/special_su /system/bin/su
-#chmod 4755 /system/bin/su
+cp /system/xbin/special_su /system/bin/su
+chmod 4755 /system/bin/su
 
 # disable cursor blinking
 echo -e '\033[?17;0;0c' > /dev/tty0
+# Set some nice mixer defaults
+
+alsa_amixer set Master 100
+alsa_amixer set Headphone 100
+alsa_amixer set Speaker 100
+alsa_amixer set 'Mic Boost' 2
+alsa_amixer set 'Capture' 25
+alsa_amixer set 'Capture' cap
+
+
+
