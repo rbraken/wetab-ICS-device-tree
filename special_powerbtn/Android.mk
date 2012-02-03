7370076ccaffeb723f515bfb0938c44eae55da73
Rene van den Braken <rene.van.den.braken@gmail.com>
2012-02-03 13:08:51 +0100

    Beter (nicer) handling of the wetab specific power button deamon We need this (later) for suspens/resume.

--- special_powerbtn/Android.mk
+++ special_powerbtn/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := special_powerbtnd
+LOCAL_MODULE := wetab_powerbtnd
 LOCAL_SRC_FILES := powerbtnd.c
 
 LOCAL_SHARED_LIBRARIES := liblog 
